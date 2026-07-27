1b758b333b7914c25a75512139e385ba0d3b9ffb
Chin-I Feng <c.feng@outlook.de>
2026-07-27 13:46:31 +0200

    Implement reusable single-step and multi-step CTRV motion

--- src/ship_trajectory_prediction/models/__init__.py
+++ src/ship_trajectory_prediction/models/__init__.py
@@ -1,4 +1,4 @@
-"""Bayesian ship trajectory prediction models."""
+"""Deterministic and Bayesian ship trajectory prediction models."""
 
 from ship_trajectory_prediction.models.constant_radius import (
     STAN_FILE,
@@ -11,13 +11,21 @@ from ship_trajectory_prediction.models.constant_turn_rate import (
     compile_constant_turn_rate_model,
     fit_constant_turn_rate_model,
 )
+from ship_trajectory_prediction.models.ctrv import (
+    CTRVState,
+    ctrv_step,
+    predict_ctrv,
+)
 
 __all__ = [
+    "CTRVState",
     "STAN_FILE",
     "build_stan_data",
     "compile_constant_radius_model",
     "compile_constant_turn_rate_model",
+    "ctrv_step",
     "fit_constant_radius_model",
     "fit_constant_turn_rate_model",
+    "predict_ctrv",
     "summarize_predictions",
 ]
